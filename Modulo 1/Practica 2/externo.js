09b1cca05f42735902a7565b13a4dd294a0459d4
victoriabrian <93290232+victoriabrian@users.noreply.github.com>
2022-03-16 15:08:12 -0300

    Add files via upload

---
+++ Modulo 1/Practica 2/externo.js
@@ -0,0 +1,13 @@
+var nombre = "Vicsdftoria"
+var apellido = "Brisdfan"
+var edad = "2dsf2"
+var mensaje = "Biedsfsdnvenido/a: "
+
+nombre = prompt("Ingrsdfese su nombre")
+apellido = prompt("Ingresfdse su apellido:")
+edad = prompt("Ingrese ssfu edad:")
+
+document.write("Usted ha sisfdo registrado con los siguientes datos")
+document.write("Nombdfre: " + nombre)
+document.write("Apedfllido: " + apellido)
+document.write("Edafd: " + edad)
